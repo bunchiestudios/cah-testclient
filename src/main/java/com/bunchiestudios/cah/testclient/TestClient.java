0d13ea587f72841362e6ca714177d964d386268e
franspaco <franspaco@gmail.com>
2016-09-17 01:53:54 -0500

    Added main

---
+++ src/main/java/com/bunchiestudios/cah/testclient/TestClient.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bunchiestudios.cah.testclient;
+import  org.json.*;
+/**
+ *
+ * @author franspaco
+ */
+public class TestClient {
+    public static void main(String[] args){
+        
+    }
+}
